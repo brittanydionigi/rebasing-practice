01d3545d3ed0bef855d9c6dad93025b174880168
Mike Limberg <limbergmike@gmail.com>
2017-10-04 13:26:00 -0600

    hi Bgit add .git add .

--- logger.js
+++ logger.js
@@ -4,3 +4,4 @@ console.log('ipsum');
 console.log('that yung jhun tho (master)');
 console.log('one more commit (master)');
 console.log('limbos latest commit tho (master)');
+console.log('hi brittany youre killing it!!!');
